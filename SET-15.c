a8732dedb42b8f6d77339e5f666b72dc77ddba8a
Sampath0610 <46046788+Sampath0610@users.noreply.github.com>
2019-02-19 16:38:07 +0530

    Create SET-15.c

---
+++ SET-15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h> 
+int main(void) 
+{
+	char a[50],c;
+	int i,j,count=1,max=1,l;
+    gets(a);
+    l=strlen(a);
+    printf("total length%d",l);
+	for(i=0;i<l;i++)
+	{
+		for(j=i+1;j<l;j++)
+		if(a[i]==a[j])
+		{
+			count++;
+			if(count>max)
+			{
+				max=count;
+				c=a[i];
+			}
+		}
+ 
+	}
+	printf("\n%c",c);
+	return 0;
+}
