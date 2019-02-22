88d29c4a05c36ddcfd2715b96e4679c18e5ea446
Sampath0610 <46046788+Sampath0610@users.noreply.github.com>
2019-02-22 15:55:12 +0530

    Create Set-11(Holidays as yes).c

---
+++ Set-11(Holidays as yes).c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[10],b[10];
+    printf("enter the days");
+    scanf("%s",a);
+    scanf("%s",b);
+    if(a=='sunday')
+    {
+    printf("yes\n");
+    }
+    else if(a=='saturday')
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    if(b=='sunday')
+    {
+    printf("yes\n");
+    }
+    else if(b=='saturday')
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+}
