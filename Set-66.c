359ffad4d0d86728071d290a4418476efd47f2dd
Sampath0610 <46046788+Sampath0610@users.noreply.github.com>
2019-02-27 21:32:31 +0530

    Create Set-66.c

---
+++ Set-66.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,k,a[50],i;
+    clrscr();
+    scanf("%d",&n);
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            printf("%d",a[i]);
+        }
+    }
+    
+}
